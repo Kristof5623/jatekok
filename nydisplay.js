56c43a990965e8f8a7b25d2541639576e65f0a67
Kristof5623 <mr.kristof18@gmail.com>
2025-11-20 13:55:35 +0100

    Add files via upload

---
+++ nydisplay.js
@@ -0,0 +1,15 @@
+let clickCountGuessing = 0;
+let clickCountRps = 0;
+
+const guessingBtn = document.getElementById("kitalalosBtn");
+const rpsBtn = document.getElementById("rpsBtn");
+
+guessingBtn.addEventListener("click", function(){
+    window.location.href = "../z kitalálos játék/kitalálos.html";
+})
+
+rpsBtn.addEventListener("click", function(){
+    window.location.href = "../rps/rps.html"
+})
+
+
